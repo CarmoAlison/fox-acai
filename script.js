f10d833df0cf9da5ec5ddf59c0b1c0eb8f2e69da
CarmoAlison <157990201+CarmoAlison@users.noreply.github.com>
2025-05-17 21:21:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -301,7 +301,7 @@ document.addEventListener('DOMContentLoaded', function() {
             case 'ESPECIAL TRUFADO':
                 return sizeInt === 400 ? 24.00 : 32.00;
             default:
-                return 25.00;
+                return 39.00;
         }
     }
     
